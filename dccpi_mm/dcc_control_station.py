7caddc5c1c2e4664c1b4f232e3706db311df19af
Max Mazur <sirmax123@gmail.com>
2019-12-11 16:15:02 +0200

    Add configurable idle packets number

--- dccpi_mm/dcc_control_station.py
+++ dccpi_mm/dcc_control_station.py
@@ -16,7 +16,7 @@ class DCCControlStation(object):
     DCCControlStation class reads commands from queue and sends on track.
     This basic implementation does not support tornout control, track-busy-sensors etc,
     """
-    def __init__(self, commands_queue, emergency_queue, **redis_kwargs):
+    def __init__(self, commands_queue, emergency_queue, idle_packets_count=10, **redis_kwargs):
 
         self.packet_factory = DCCPacketFactory()
         self.idle_packet = self.packet_factory.DCCIdlePacket().to_bit_string()
@@ -29,13 +29,14 @@ class DCCControlStation(object):
 
         self.logger = getLogger('DCCControlStation')
         self.logger.debug('DCCControlStation init')
+        self.idle_packets_count = idle_packets_count
 
     def decode_command(self, command_json):
         """
         Method reads command from queues and sends on track.
         IF emergency_queue is NOT EMPTY eStop (emergency stop) packet will be saend on track
         """
-        self.logger.debug("Got DCC Packet from queue: {command_json}".format(command_json=command_json))
+        self.logger.info("Got DCC Command from queue: {command_json}".format(command_json=command_json))
         command = json.loads(command_json)
         try:
             if command['action'] == 'move':
@@ -43,10 +44,10 @@ class DCCControlStation(object):
                             locoAddress = command['loco_address'],
                             speedDirection = {
                                 "speed": command['speed'],
-                                "direction": command['direction']}).to_bit_string(), 3)
+                                "direction": command['direction']}).to_bit_string(), 1)
             elif command['action'] == 'functon':
                 self.hardware.send_bit_string(self.packet_factory.DCCFunctionPacket(locoAddress = command['loco_address'],
-                                            functionsState = command['functions_state']).to_bit_string(), 3)
+                                            functionsState = command['functions_state']).to_bit_string(), 1)
 
         except KeyboardInterrupt:
             sys.exit(1)
@@ -58,12 +59,12 @@ class DCCControlStation(object):
         while True:
             while  not self.emergency_queue.empty():
                 emergency_command = self.emergency_queue.get()
-                self.logger.debug("Emergency Command = {emergency_command}".format(emergency_command=emergency_command))
+                self.logger.info("Emergency Command = {emergency_command}".format(emergency_command=emergency_command))
                 self.hardware.send_bit_string(self.e_stop_packet, 3)
 
             if not self.commands_queue.empty():
-                command_json = self.commands_queue.get()
-                self.logger.debug("Command = {command_json}".format(command_json=command_json))
+                command_json = self.commands_queue.get().decode('utf-8')
+                self.logger.info("Command = {command_json}".format(command_json=command_json))
                 self.decode_command(command_json)
 
-            self.hardware.send_bit_string(self.idle_packet, 1)
+            self.hardware.send_bit_string(self.idle_packet, self.idle_packets_count)
